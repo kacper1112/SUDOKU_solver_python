70689063233ac3774a0b337175987d11eadc2c7c
Kacper Stysiński <kaacper1112@gmail.com>
2019-12-22 00:32:15 +0100

    solver complete

--- sudoku.py
+++ sudoku.py
@@ -1,6 +1,9 @@
+import copy
+
 grid = [[0] * 9 for _ in range(9)]  # main sudoku grid
 
 # grid[row][col]
+# grid[ i ][ j ]
 
 def checkPos(grid, row, col, val):
     for i in range(9):
@@ -14,8 +17,11 @@ def checkPos(grid, row, col, val):
     startCol = col // 3
     startRow = row // 3
 
-    for i in range(startRow, startRow + 3):
-        for j in range(startCol, startCol + 3):
+    #print(startCol * 3, startCol * 3 + 3)
+    #print(startRow * 3, startRow * 3 + 3)
+
+    for i in range(startRow * 3, startRow * 3 + 3):
+        for j in range(startCol * 3, startCol * 3 + 3):
             if grid[i][j] == val:
                 return False
 
@@ -23,9 +29,33 @@ def checkPos(grid, row, col, val):
 
 
 def solve(grid):
-    pass
+    empty = []
 
+    for i in range(9):
+        for j in range(9):
+            if grid[i][j] == 0:
+                empty.append([i, j])
 
+    return helper(grid, empty)
+
+def helper(grid, empty):
+
+    if len(empty) == 0:
+        return grid
+    
+    i, j = empty[0][0], empty[0][1]
+    
+    for val in range(1, 10):
+        if checkPos(grid, i, j, val) is True:
+            tempGrid = copy.deepcopy(grid)
+            tempGrid[i][j] = val
+            
+            result = helper(tempGrid, empty[1:])
+
+            if result is not False:
+                return result
+
+    return False
 
 def printGrid(grid):
     for i in range(9):
@@ -42,5 +72,9 @@ def printGrid(grid):
 for i in range(9):
     grid[i] = [int(n) for n in input().split()]
 
+solved = solve(grid)
 
-printGrid(grid)
+if solved is not False:
+    printGrid(solved)
+else:
+    print("Unable to solve")
